fb63ef0f16322059dd96039512eee0550d04e3cc
Jacob0312 <jaenk0312@gmail.com>
2022-05-15 20:52:38 +0200

    React JS Masterclass beginning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
+const Father = styled.div`
+	display: flex;
+`;
+
+const Box = styled.div`
+	background-color: ${(props) => props.bgColor};
+	width: 100px;
+	height: 100px;
+`;
+
+const Circle = styled(Box)`
+	border-radius: 50px;
+	background-color: green;
+`;
+
+const Text = styled.span`
+	color: white;
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Father>
+			<Box bgColor="tomato">
+				<Text>Hi</Text>
+			</Box>
+			<Circle></Circle>
+			<Box bgColor="teal"></Box>
+		</Father>
+	);
 }
 
 export default App;
